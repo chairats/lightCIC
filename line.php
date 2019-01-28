84c581836f032f8b39ec3cd7fc36931c22ac7479
chairats <47059287+chairats@users.noreply.github.com>
2019-01-28 12:30:57 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '4IYIQ+rbApF9bRIi1PmfbaG2Tp901+T+9Qh7UsVxsp5cQ/8Hd+RYYn+5UV8y+JEdqUWQRVm+eOqGUYV4xhlw27sjRB42NEfVgZc/UQ6kSQf9PD7ooCQqDV2EI3vTdr8JhyHjpv82/4sTwi93m/ygoQdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U857cece0f471d189abad6aae7bdacd9f',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
